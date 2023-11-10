82b57db29ef0e8f78432891de5f4341e3cf4bdac
Jayanth-Parthsarathy <jparthaa@gmail.com>
2023-11-10 11:41:54 +0530

    Change some styles for company page

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -6,21 +6,21 @@ const CompanyPage = async () => {
   const session = await getServerAuthSession();
   if (!session?.user) {
     return (
-        <h1 className="mb-10 text-3xl mt-52 font-semibold text-center">
-          You need to login before you can continue
-        </h1>
+      <h1 className="mb-10 mt-52 text-center text-3xl font-semibold">
+        You need to login before you can continue
+      </h1>
     );
   }
 
   return (
-    <div className="mx-auto my-60 flex h-96 w-96 flex-col items-center justify-center gap-10 rounded-lg shadow-md">
+    <div className="flex h-screen flex-col items-center justify-center">
       <h1 className="mb-10 text-2xl font-semibold">Create or Join a company</h1>
       <div className="flex gap-4">
         <Link href="/company/create">
-          <Button>Create company</Button>{" "}
+          <Button>Create company</Button>
         </Link>
         <Link href="/company/join">
-          <Button>Join company</Button>{" "}
+          <Button>Join company</Button>
         </Link>
       </div>
     </div>
